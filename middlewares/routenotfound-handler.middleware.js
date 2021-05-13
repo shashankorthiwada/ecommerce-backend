af06a241023c936cf11d2015236cc137e155f21c
Shashank Korthiwada <skorthiwada@manh.com>
2021-05-13 13:15:32 +0530

    refactor: refactored files from main

--- middlewares/routenotfound-handler.middleware.js
+++
@@ -1,8 +0,0 @@
-const routeNotFound = (req, res, next) => {
-  res.status(404).json({
-    success: false,
-    message: "the route which you are looking for is not found",
-  });
-};
-
-module.exports = { routeNotFound };
